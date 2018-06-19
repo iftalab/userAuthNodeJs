9a24ece0c18320805485ba2aa2856118be620ca5
voldemort <ifta.noor@revesoft.com>
2018-06-19 22:19:42 +0600

    email validation in model added

--- api/v1/model/user.js
+++ api/v1/model/user.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    _id : mongoose.Schema.Types.ObjectId,
-    updated : {type : Date, default : Date.now()},
-	email : {type : String, required : true},
-	password :  {type : String, required : true},
-	fullName : String,
-	imageUrl : String,
+	_id: mongoose.Schema.Types.ObjectId,
+	updated: { type: Date, default: Date.now() },
+	email: {
+		type: String,
+		required: true,
+		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+		
+	},
+	password: { type: String, required: true },
+	fullName: String,
+	imageUrl: String,
 });
-module.exports = mongoose.model('User',userSchema);
+
+module.exports = mongoose.model('User', userSchema);
